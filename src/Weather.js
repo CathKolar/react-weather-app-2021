e924d3b42703c50de767962bb5a876b8fcf11201
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-04-07 13:06:54 +0200

    display correct icon

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather({ defaultCity }) {
       windSpeed: response.data.wind.speed,
       windDirection: response.data.wind.deg,
       description: response.data.weather[0].description,
-      img: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      img: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
